4b484468273bd7be7f61088855be24381982acb7
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-02-22 16:42:41 +0100

    start DOM selectors and Eventlisteners, add fake-fetch function to test

--- code/script.js
+++ code/script.js
@@ -1,6 +1,10 @@
 const RECIPE_URL =
   "https://api.edamam.com/search?q=chicken&app_id=38ba247d&app_key=21b567898d4c629acb5a6436b7b5abc2&from=0&to=3&calories=591-722&health=alcohol-free";
-  
+const searchDesktop = document.getElementById('searchDesktop')
+const searchMobile = document.getElementById('searchMobile')
+const submitButtonMobile = document.getElementById('submit-button-mobile')
+const submitButtonDesktop = document.getElementById('submit-button-desktop')
+
 console.log('hello!')
 
 const fetchRealData = () => {
@@ -30,5 +34,19 @@ const fetchRealData = () => {
     recipes.innerHTML = `${error}`;
   });
 }
+
+const fetchData = (data) => {
+  console.log(data)
+}
+
+submitButtonMobile.addEventListener('click', event => {
+  event.preventDefault();
+  fetchData (searchMobile.value)
+})
+
+submitButtonDesktop.addEventListener('click', event => {
+  event.preventDefault();
+  fetchData (searchDesktop.value)
+})
 
 console.log(dummyResponse.hits[0].recipe.label);
